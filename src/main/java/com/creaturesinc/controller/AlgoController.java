581ffaac52ea4417c0dfab3dc19ce970f19df4b5
santiagoAlgo <37193161+santiagoAlgo@users.noreply.github.com>
2020-08-16 16:19:44 -0500

    Update AlgoController.java

--- src/main/java/com/creaturesinc/controller/AlgoController.java
+++ src/main/java/com/creaturesinc/controller/AlgoController.java
@@ -2,6 +2,7 @@ package com.creaturesinc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class AlgoController {
