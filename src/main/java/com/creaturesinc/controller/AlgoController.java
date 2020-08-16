8b83302a274f4b7ee1d46b926897121382c11803
santiagoAlgo <37193161+santiagoAlgo@users.noreply.github.com>
2020-08-16 17:53:56 -0500

    jijifewfewfew

--- src/main/java/com/creaturesinc/controller/AlgoController.java
+++ src/main/java/com/creaturesinc/controller/AlgoController.java
@@ -14,11 +14,17 @@ public class AlgoController {
 
 	@Value("${algo.secret.question}")
 	private String questionDisp;
+	
+	@Value("${algo.db.url}")
+	private String dbUrl;
+	
+	@Value("${algo.db.user}")
+	private String dbUser;
 
 	@GetMapping("/algo")
 	public String getAlgo() {
 		//return "algo 2 "+env.getProperty("agent007");
-		return "algo 3 "+name+"<br>"+questionDisp;
+		return "algo 3 "+name+"<br>"+questionDisp+"<br><ul><il>"+dbUrl+"</il><il>"+dbUser+"</il></ul>";
 	} 
 	
 }
