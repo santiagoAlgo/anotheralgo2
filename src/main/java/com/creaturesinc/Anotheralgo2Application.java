fc2842c28e876388df6072c429a016579fc88b25
santiagoAlgo <37193161+santiagoAlgo@users.noreply.github.com>
2020-08-16 13:56:17 -0500

    kkokop

---
+++ src/main/java/com/creaturesinc/Anotheralgo2Application.java
@@ -0,0 +1,13 @@
+package com.creaturesinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Anotheralgo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Anotheralgo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/creaturesinc/Anotheralgo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creaturesinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Anotheralgo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
